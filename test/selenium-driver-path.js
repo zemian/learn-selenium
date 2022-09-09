2ffa966a4aac43b568e85ddf2d98f38c0342a7c4
Zemian Deng <zemian.deng@oracle.com>
2022-09-09 11:37:40 -0400

    Add driver path debug script

---
+++ test/selenium-driver-path.js
@@ -0,0 +1,31 @@
+// How to verify chromedriver path?
+// https://stackoverflow.com/questions/26191142/selenium-nodejs-chromedriver-path
+
+const {Builder, By, Key, until, Capabilities} = require('selenium-webdriver');
+
+var chrome = require('selenium-webdriver/chrome');
+//var path = require('chromedriver').path;
+
+it("selenium google search example", async function() {
+  this.timeout(60000);
+
+  // Note: this will initialize code to check Chrome browser version match to driver
+  //let driver = await new Builder().forBrowser('chrome').build();
+  //console.log(driver);
+
+  //console.log(chrome);
+  // console.log(path);
+
+  // Use custom driver path
+  const path = '/Users/zemian/bin/chromedriver_105.0.5195.52';
+  var service = new chrome.ServiceBuilder(path).build();
+  chrome.setDefaultService(service);
+
+  // var driver = new Builder()
+  //   .withCapabilities(Capabilities.chrome())
+  //   .build();
+  // //console.log(driver);
+
+  let driver = await new Builder().forBrowser('chrome').build();
+  driver.quit();
+});
