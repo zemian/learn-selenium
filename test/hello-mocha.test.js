2515dbb76965ede81266c7a693fa0e1f06373317
Zemian Deng <zemian.deng@oracle.com>
2022-09-09 10:53:34 -0400

    Add more mocha tests

--- test/hello-mocha.test.js
+++ test/hello-mocha.test.js
@@ -29,6 +29,45 @@ it("mocha return promise example", function() {
   });
 });
 
+it("mocha multiple await work example#1", function() {
+  return new Promise(async (resolve) => {
+    await new Promise(resolve => setTimeout(resolve, 100));
+    await new Promise(resolve => setTimeout(resolve, 200));
+    await new Promise(resolve => setTimeout(resolve, 300));
+    resolve();
+  });
+});
+
+it("mocha multiple await work example#2", function(done) {
+  // Do not return the promise, but use 'done' callback instead.
+  new Promise(async (resolve) => {
+    await new Promise(resolve => setTimeout(resolve, 100));
+    await new Promise(resolve => setTimeout(resolve, 200));
+    await new Promise(resolve => setTimeout(resolve, 300));
+    resolve(done());
+  });
+});
+
+/*
+// Below will not work! Mocha requires either "async function" or "new Promise"
+// or function(done) is used only, but not both combined.
+it("mocha multiple await work example#3", async function(done) {
+  // Do not use promise, but use 'done' callback instead.  
+  await new Promise(resolve => setTimeout(resolve, 100));
+  await new Promise(resolve => setTimeout(resolve, 200));
+  await new Promise(resolve => setTimeout(resolve, 300));
+  done();
+});
+*/
+
+it("mocha multiple await work example#4", async function() {
+  // Do not use promise, but use 'done' callback instead.  
+  await new Promise(resolve => setTimeout(resolve, 100));
+  await new Promise(resolve => setTimeout(resolve, 200));
+  await new Promise(resolve => setTimeout(resolve, 300));
+});
+
+
 it("mocha string concat example", function() {
   assert.deepEqual("a" + "b", "ab"); 
 });
